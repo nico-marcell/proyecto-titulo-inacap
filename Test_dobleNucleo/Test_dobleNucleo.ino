946d42ada017b35e799f3c5e1480a08de81d2f37
nico-marcell <128751089+nico-marcell@users.noreply.github.com>
2024-12-17 04:07:48 -0300

    Add files via upload

---
+++ Test_dobleNucleo/Test_dobleNucleo.ino
@@ -0,0 +1,52 @@
+#include <Arduino.h>
+
+TaskHandle_t Task2, Task3;
+int cuenta = 0;
+
+void loop2(void *parameter){
+  for(;;){
+    Serial.println("\t\t\tEn núcleo -> " +  String(xPortGetCoreID()));
+    delay(1000);
+    cuenta++;
+    Serial.println("desde loop 2 -> " + String(cuenta));
+  }
+  vTaskDelay(100);
+}
+
+void loop3(void *parameter){
+  for(;;){
+    Serial.println("\t\t\t Loop 3 🤯🤯🤯");
+    delay(2000);
+    cuenta++;
+  }
+  vTaskDelay(100);
+}
+
+void setup() {
+
+  xTaskCreatePinnedToCore(
+    loop2,
+    "Task_2",
+    1000,
+    NULL,
+    1,
+    &Task2,
+    0);
+
+  xTaskCreatePinnedToCore(
+      loop3,
+      "Task_3",
+      1000,
+      NULL,
+      1,
+      &Task3,
+      0);
+
+  Serial.begin(115200);
+}
+
+void loop() {
+  Serial.println("En núcleo -> " +  String(xPortGetCoreID()));
+  delay(1500);
+  cuenta++;
+}
